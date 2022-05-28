7391a0fd68e5c90c6722bdbcfe450fa87fbf8d27
Ean Milligan (Bastion) <ean.milligan@gmail.com>
2022-05-28 02:07:23 -0400

    deno fmt

--- src/commands/auditCmd/auditHelp.ts
+++ src/commands/auditCmd/auditHelp.ts
@@ -17,17 +17,19 @@ export const auditHelp = (message: DiscordenoMessage) => {
 				{
 					name: `\`${config.prefix}audit help\``,
 					value: 'This command',
-					inline: true
-				}, {
+					inline: true,
+				},
+				{
 					name: `\`${config.prefix}audit db\``,
 					value: 'Shows current DB table sizes',
-					inline: true
-				}, {
+					inline: true,
+				},
+				{
 					name: `\`${config.prefix}audit guilds\``,
 					value: 'Shows breakdown of guilds and detials on them',
-					inline: true
+					inline: true,
 				},
-			]
+			],
 		}],
 	}).catch((e) => {
 		log(LT.ERROR, `Failed to send message: ${JSON.stringify(message)} | ${JSON.stringify(e)}`);
