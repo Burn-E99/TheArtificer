18c5d2a23c07fbb7d61146c5eb0d92e8cf72cf17
Ean Milligan (Bastion) <ean.milligan@gmail.com>
2022-07-08 01:31:13 -0400

    update heatmap.png

--- src/endpoints/gets/heatmapPng.ts
+++ src/endpoints/gets/heatmapPng.ts
@@ -6,11 +6,14 @@ import {
 
 export const heatmapPng = async (requestEvent: Deno.RequestEvent) => {
 	const file = Deno.readFileSync('./src/endpoints/gets/heatmap.png');
+	const imageHeaders = new Headers();
+	imageHeaders.append('Content-Type', 'image/png');
 	// Send basic OK to indicate key has been sent
 	requestEvent.respondWith(
 		new Response(file, {
 			status: Status.OK,
 			statusText: STATUS_TEXT[Status.OK],
+			headers: imageHeaders,
 		}),
 	);
 };
