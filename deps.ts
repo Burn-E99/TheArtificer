ef08cd779aa2e3c9072d3f8fd08cb8c1aff96d53
Ean Milligan <ean.milligan@gmail.com>
2025-04-26 13:18:05 -0400

    deno upgrade

--- deps.ts
+++ deps.ts
@@ -1,26 +1,28 @@
 // All external dependancies are to be loaded here to make updating dependancy versions much easier
 export {
-	botId,
-	cache,
-	cacheHandlers,
-	DiscordActivityTypes,
-	editBotNickname,
-	editBotStatus,
-	hasGuildPermissions,
-	Intents,
-	sendDirectMessage,
-	sendMessage,
-	startBot,
+  botId,
+  cache,
+  cacheHandlers,
+  DiscordActivityTypes,
+  editBotNickname,
+  editBotStatus,
+  hasGuildPermissions,
+  Intents,
+  sendDirectMessage,
+  sendMessage,
+  startBot,
 } from 'https://deno.land/x/discordeno@12.0.1/mod.ts';
 
 export type { CreateMessage, DiscordenoGuild, DiscordenoMessage, EmbedField } from 'https://deno.land/x/discordeno@12.0.1/mod.ts';
 
-export { Client } from 'https://deno.land/x/mysql@v2.10.2/mod.ts';
+export { Client } from 'https://deno.land/x/mysql@v2.12.1/mod.ts';
 
-export { Status, STATUS_TEXT } from 'https://deno.land/std@0.145.0/http/http_status.ts';
+export { STATUS_CODE, STATUS_TEXT } from 'jsr:@std/http@1.0.15';
+
+export type { StatusCode } from 'jsr:@std/http@1.0.15';
 
 export { nanoid } from 'https://deno.land/x/nanoid@v3.0.0/mod.ts';
 
 export { initLog, log, LogTypes as LT } from 'https://raw.githubusercontent.com/Burn-E99/Log4Deno/V1.1.1/mod.ts';
 
-export * as is from 'https://deno.land/x/imagescript@v1.2.13/mod.ts';
+export * as is from 'https://deno.land/x/imagescript@1.3.0/mod.ts';
