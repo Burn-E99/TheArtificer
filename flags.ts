a01779399760f0e0b6a14f3f42ed848c9c208317
Ean Milligan <ean.milligan@gmail.com>
2025-04-26 21:11:56 -0400

    update comments

--- flags.ts
+++ flags.ts
@@ -1,6 +1,6 @@
 // DEVMODE is to prevent users from accessing parts of the bot that are currently broken
 export const DEVMODE = false;
-// DEBUG is used to toggle the cmdPrompt
+// DEBUG is used to toggle the cmdPrompt and show debug log messages
 export const DEBUG = false;
-// LOCALMODE is used to run a differnt bot token for local testing
+// LOCALMODE is used to run a different bot token for local testing
 export const LOCALMODE = false;
