cb00147f6bfd8d02c530f6deab67886a8146d6cb
Ean Milligan <ean.milligan@gmail.com>
2025-05-01 18:01:54 -0400

    [untested] whoops wasn't supposed to add that...

--- flags.ts
+++ flags.ts
@@ -3,4 +3,4 @@ export const DEVMODE = false;
 // DEBUG is used to toggle the cmdPrompt and show debug log messages
 export const DEBUG = false;
 // LOCALMODE is used to run a different bot token for local testing
-export const LOCALMODE = true;
+export const LOCALMODE = false;
