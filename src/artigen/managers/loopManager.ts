9bd757741b6517c6f156e94280eeb229dedfd8a6
Ean Milligan <ean.milligan@gmail.com>
2025-05-03 09:25:59 -0400

    remove logging from loopmanager

--- src/artigen/managers/loopManager.ts
+++ src/artigen/managers/loopManager.ts
@@ -1,11 +1,6 @@
-import { log, LogTypes as LT } from '@Log4Deno';
-
 import config from '~config';
 
-import { loggingEnabled } from 'src/artigen/utils/logFlag.ts';
-
 let loopCount = 0;
-loggingEnabled && log(LT.LOG, 'Loop Manager Initialized');
 
 // Will ensure if maxLoops is 10, 10 loops will be allowed, 11 will not.
 export const loopCountCheck = (): void => {
