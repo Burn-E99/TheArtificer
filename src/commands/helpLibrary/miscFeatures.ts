4b6683525ca8518c8a41882c616624f5372e06fb
Ean Milligan <ean.milligan@gmail.com>
2025-07-08 02:26:10 -0400

    add group help pages

--- src/commands/helpLibrary/miscFeatures.ts
+++ src/commands/helpLibrary/miscFeatures.ts
@@ -38,6 +38,56 @@ Damage: 14`,
     },
   ],
   [
+    'single-mode-group',
+    {
+      name: 'Single Sub-Roll Group',
+      description: `${config.name} supports Roll20's Grouped Roll syntax.  This page details how the single sub-roll mode works, and how the options interact with the dice rolled in it.
+
+When no options are provided, the group is effectively transparent and does not have any affect on the roll.
+
+The following options are supported at a group level: Drop (Lowest), Drop Highest, Keep (Highest), Keep Lowest, Target Number (Successes) and Target Failures
+Specifically: \`d\`, \`dl\`, \`dh\`, \`k\`, \`kl\`, \`kh\`, \`<\`, \`>\`, \`=\`, \`f\`, \`f<\`, \`f>\`, and \`f=\`
+
+The group variants of these share the same rules as the normal Dice Options, and more details can be found in the \`Dice Options\` help section.
+
+When in Single Sub-Roll mode, the group options are applied across all sub-rolls.  This means, for example, you can roll a few different sized dice at the same time and only keep the highest rolls from that combination of dice.`,
+      example: [
+        '`[[{4d6 + 3d8}]]` => {[__1__ + 4 + __1__ + 3]+[5 + 3 + 5]} = __22__',
+        '`[[{4d6 + 3d8}d2]]` => {[**6** + **6** + ~~2~~ + 4]+[6 + ~~__1__~~ + 7]} = **29**',
+        '`[[{4d6 + 3d8}k4]]` => {[4 + ~~__1__~~ + 5 + ~~2~~]+[**8** + ~~__1__~~ + **8**]} = **25**',
+        '`[[{4d6 + 3d8}>5]]` => {[3 + 3 + S:5 + S:5, 2 Successes]+[**S:8** + 3 + __1__, 1 Success]} = **__3__**',
+        '`[[{4d6 + 3d8}f<3]]` => {[**6** + 5 + __F:1__ + **6**, 1 Fail]+[5 + F:2 + 4, 1 Fail]} = **__-2__**',
+        '`[[{4d6 + 3d8}>5f<3]]` => {[__F:1__ + S:5 + F:3 + 4, 1 Success, 2 Fails]+[__F:1__ + __F:1__ + 4, 0 Successes, 2 Fails]} = __-3__',
+        '`[[{4d6 + 3d8}k5>6f<4]]` => {[**S:6** + ~~3~~ + 5 + F:4, 1 Success, 1 Fail]+[F:4 + ~~2~~ + **S:8**, 1 Success, 1 Fail]} = **0**',
+      ],
+    },
+  ],
+  [
+    'multi-mode-group',
+    {
+      name: 'Multi Sub-Roll Group',
+      description: `${config.name} supports Roll20's Grouped Roll syntax.  This page details how the multi sub-roll mode works, and how the options interact with the dice rolled in it.
+
+When no options are provided, the group is put into sum mode and sums each sub-roll into one total.
+
+The following options are supported at a group level: Drop (Lowest), Drop Highest, Keep (Highest), Keep Lowest, Target Number (Successes) and Target Failures
+Specifically: \`d\`, \`dl\`, \`dh\`, \`k\`, \`kl\`, \`kh\`, \`<\`, \`>\`, \`=\`, \`f\`, \`f<\`, \`f>\`, and \`f=\`
+
+The group variants of these share the same rules as the normal Dice Options, and more details can be found in the \`Dice Options\` help section.
+
+When in Multi Sub-Roll mode, the group options are applied to the results of each sub-roll.  This means, for example, you can sum the highest sub-roll results or set a specific threshold for overall successes.`,
+      example: [
+        '`[[{4d6+2d8, 3d20+3, 5d10+1}]]` => {[2 + 3 + __1__ + 3]+[5 + 5] + [19 + 3 + 8]+3 + [**10** + 6 + 6 + **10** + 2]+1} = **__87__**',
+        '`[[{4d6+2d8, 3d20+3, 5d10+1}d1]]` => {[3 + **6** + 3 + __1__]+[3 + 6], ~~[__1__ + 10 + 5]+3~~, [7 + __1__ + 7 + 7 + __1__]+1} = **__46__**',
+        '`[[{4d6+2d8, 3d20+3, 5d10+1}k1]]` => {~~[__1__ + **6** + 4 + 3]+[5 + 4]~~, ~~[5 + 2 + 19]+3~~, [2 + 9 + 9 + 5 + 5]+1} = 31',
+        '`[[{4d6+2d8, 3d20+3, 5d10+1}>30]]` => {[__1__ + 2 + 2 + 5]+[6 + 2], [15 + 7 + 4]+3, S:[**10** + 9 + 4 + **10** + 6]+1, 1 Success} = **__1__**',
+        '`[[{4d6+2d8, 3d20+3, 5d10+1}f<25]]` => {F:[2 + 5 + __1__ + 2]+[__1__ + 4], [17 + 5 + 5]+3, [8 + 9 + 6 + 4 + 7]+1, 1 Fail} = __-1__',
+        '`[[{4d6+2d8, 3d20+3, 5d10+1}>30f<25]]` => {F:[5 + 2 + 3 + **6**]+[__1__ + 7], S:[12 + 12 + 10]+3, [6 + 9 + __1__ + 6 + 5]+1, 1 Success, 1 Fail} = **__0__**',
+        '`[[{4d6+2d8, 3d20+3, 5d10+1}k2>30f<25]]` => {~~[2 + 4 + 4 + 2]+[__1__ + 2]~~, S:[4 + 8 + **20**]+3, [4 + __1__ + **10** + 3 + 8]+1, 1 Success, 0 Fails} = **__1__**',
+      ],
+    },
+  ],
+  [
     'variable',
     {
       name: 'Variables',
