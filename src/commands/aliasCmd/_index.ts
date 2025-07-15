5233d029f31be19591b67f0fdc488bccfb6cf29a
Ean Milligan <ean.milligan@gmail.com>
2025-07-15 01:24:28 -0400

    alias import update

--- src/commands/aliasCmd/_index.ts
+++ src/commands/aliasCmd/_index.ts
@@ -1,9 +1,9 @@
-import { add, update } from './aliasAddUpdate.ts';
-import { clone } from './clone.ts';
-import { deleteAll, deleteOne } from './aliasDelete.ts';
+import { add, update } from 'commands/aliasCmd/aliasAddUpdate.ts';
+import { clone } from 'commands/aliasCmd/clone.ts';
+import { deleteAll, deleteOne } from 'commands/aliasCmd/aliasDelete.ts';
 import { help } from 'commands/aliasCmd/aliasHelp.ts';
 import { list } from 'commands/aliasCmd/list.ts';
-import { rename } from './rename.ts';
+import { rename } from 'commands/aliasCmd/rename.ts';
 import { run } from 'commands/aliasCmd/run.ts';
 import { view } from 'commands/aliasCmd/view.ts';
 
