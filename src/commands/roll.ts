65fc9e56de374691a43bc503168d93809454bc3f
Ean Milligan <ean.milligan@gmail.com>
2025-07-17 02:51:48 -0400

    remove devmode flag

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -2,7 +2,6 @@ import { DiscordenoMessage } from '@discordeno';
 import { log, LogTypes as LT } from '@Log4Deno';
 
 import config from '~config';
-import { DEVMODE } from '~flags';
 
 import { getModifiers } from 'artigen/dice/getModifiers.ts';
 
@@ -13,8 +12,6 @@ import { generateRollError, rollingEmbed } from 'artigen/utils/embeds.ts';
 import dbClient from 'db/client.ts';
 import { queries } from 'db/common.ts';
 
-import { warnColor } from 'embeds/colors.ts';
-
 import utils from 'utils/utils.ts';
 
 export const roll = async (message: DiscordenoMessage, args: string[], command: string) => {
@@ -22,21 +19,6 @@ export const roll = async (message: DiscordenoMessage, args: string[], command: 
   const currDateTime = new Date();
   dbClient.execute(queries.callIncCnt('roll')).catch((e) => utils.commonLoggers.dbError('roll.ts:20', 'call sproc INC_CNT on', e));
   dbClient.execute(queries.callIncHeatmap(currDateTime)).catch((e) => utils.commonLoggers.dbError('roll.ts:21', 'update', e));
-
-  // If DEVMODE is on, only allow this command to be used in the devServer
-  if (DEVMODE && message.guildId !== config.devServer) {
-    message
-      .send({
-        embeds: [
-          {
-            color: warnColor,
-            title: 'Command is in development, please try again later.',
-          },
-        ],
-      })
-      .catch((e: Error) => utils.commonLoggers.messageSendError('roll.ts:30', message, e));
-    return;
-  }
 
   // Rest of this command is in a try-catch to protect all sends/edits from erroring out
   try {
